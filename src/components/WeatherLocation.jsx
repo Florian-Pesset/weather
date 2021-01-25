18bcff1f91ce213b49014d12a3995f0ce61268aa
Florian-Pesset <florianpesset@gmail.com>
2021-01-25 16:22:01 +0100

    add audio

--- src/components/WeatherLocation.jsx
+++ src/components/WeatherLocation.jsx
@@ -1,25 +1,36 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import CityContext from "../contexts/CityContext";
 import styles from "../css/weatherLocation.module.css";
 import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
+import MicIcon from "@material-ui/icons/Mic";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
 const WeatherLocation = () => {
   const [userInput, setUserInput] = useState("");
   const { setCity } = useContext(CityContext);
+  const { transcript } = useSpeechRecognition();
+
+  useEffect(() => {
+    if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+      alert("Ups, your browser is not supported!");
+    }
+  }, []);
 
   const onChange = (e) => {
     setUserInput(e.target.value);
   };
 
   const adduserInput = () => {
-    setCity(userInput);
+    setCity(transcript || userInput);
   };
 
   const handleKeyPress = (e) => {
     if (e.key === "Enter") {
-      setCity(userInput);
+      setCity(transcript || userInput);
     }
   };
   const useStyles = makeStyles(() => ({
@@ -38,9 +49,11 @@ const WeatherLocation = () => {
     <div className={styles.location}>
       <h1>WeatherWorld</h1>
       <div className={styles.container}>
+        <MicIcon onClick={SpeechRecognition.startListening} fontSize="large" />
+
         <TextField
           label="Enter your city"
-          value={userInput}
+          value={transcript || userInput}
           onChange={onChange}
           onKeyPress={handleKeyPress}
           variant="filled"
